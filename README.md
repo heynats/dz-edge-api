a5eb965844f9f2dbaefc5efd9a989027468f2b03
Stanley Yeh <stanley.yeh@thingularity.co>
2021-03-02 10:55:02 +0800

    Enable event ID '0' again in updateJob

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
+# DZ-Edge-API
+
 [![Go Report Card](https://goreportcard.com/badge/github.com/istanleyy/dz-edge-api)](https://goreportcard.com/report/github.com/istanleyy/dz-edge-api)
 [![Release](https://img.shields.io/github/v/release/istanleyy/dz-edge-api?include_prereleases)](https://github.com/istanleyy/dz-edge-api/releases)
-
-# DZ-Edge-API
 
 DZ Edge API is an API server based on Gin that interacts with shop floor control systems (PLC's) to synchronize data with iShopFloor MOM solution.
 
@@ -9,22 +9,28 @@ DZ Edge API is an API server based on Gin that interacts with shop floor control
 
 To build and run DZ-Edge-API, you need to install Go first (**version 1.11+ is required**).
 Once you have setup Go working environment, you can clone and build the repo with:
-```
+
+```bash
 # clone the project from GitHub
 $ git clone https://github.com/istanleyy/dz-edge-api.git
 
 # build the project and save executable to the build/ directory
 $ cd dz-edge-api
 $ go build -o build
+
+# cross compiling for Windows can use the following command
+$ GOOS=windows go build -o build
 ```
 
 ## Quick Start
 
 Follow the above build procedure, you may find the executable file in the build folder located under the project root directory. Start the DZ-Edge-API server with:
-```
+
+```bash
 # assuming at project root directory
 $ ./dz-edge-api
 ```
+
 This starts the API server at localhost:8080. By default, current version will look for WebAccess API server on the same machine running DZ-Edge-API. If WebAccess API server is unreachable when DZ-Edge-API launches, it will keep retrying to establish the connection. API endpoints will become available only when the connection with WebAccess is established.
 
 ## API Examples
@@ -34,31 +40,37 @@ This starts the API server at localhost:8080. By default, current version will l
 CreateJob adds a new job with the following fields in the request body.
 
 Sample request body:
-```
+
+```json
 {
     "woID": "5edf6dcfc5dbda7ac93c7f19",
     "process": "gal"
 }
 ```
+
 - "woID" - 24-char work ID
 - "process" - 'pre' for 'pre-process', and 'gal' for galvanization
 
 Sample HTTP 200 response body:
-```
+
+```json
 {
     "queueIndex": 0,
     "woID": "gbe732fd831d6cea6b38d256"
 }
 ```
+
 - "queueIndex" - position of the work in queue
 - "woID" - 24-char work ID
 
 Sample HTTP error body:
-```
+
+```json
 {
     "message": "job exists in galvanizing process queue"
 }
 ```
+
 - "message" - the error message
 
 ### POST /job/update
@@ -66,34 +78,40 @@ Sample HTTP error body:
 UpdateJob updates the job status. Fields for the request body.
 
 Sample request body:
-```
+
+```json
 {
     "woID": "5edf6dcfc5dbda7ac93c7f19",
     "process": "gal",
     "eventID": 0
 }
 ```
+
 - "woID" - 24-char work ID
 - "process" - 'pre' for 'pre-process', and 'gal' for galvanization
 - "eventID" - the event code to update the job with, 0(finish order), 10(weight empty bucket), 11(weight full bucket), 12(weight good product)
 
 Sample HTTP 200 response body:
-```
+
+```json
 {
     "queueIndex": 0,
     "woID": "gbe732fd831d6cea6b38d256"
 }
 ```
+
 - "queueIndex" - position of the work in queue
 - "woID" - 24-char work ID
 
 Sample HTTP error body:
-```
+
+```json
 {
     "code": "u001",
     "message": "unknown EventID for the pre-treatment process"
 }
 ```
+
 - "code" - the error code, 'u001' refers to DZ-Edge-API error, 'u002' refers to iShopFloor API error, 'u003' refers to WebAccess API error
 - "message" - the error message
 
@@ -102,14 +120,17 @@ Sample HTTP error body:
 GetJob responds with the data in the queue identified by the uri parameters.
 
 Sample request:
+
+```bash
+curl http://localhost:8080/api/v1/job/gbe732fd831d6cea6b38d254/pre
 ```
-$ curl http://localhost:8080/api/v1/job/gbe732fd831d6cea6b38d254/pre
-```
+
 - "jobId" - 24-char work ID
 - "processType" - 'pre' for 'pre-process', and 'gal' for galvanization
 
 Sample HTTP 200 response body for 'pre':
-```
+
+```json
 {
     "process": "Pre-treatment",
     "woID": "gbe732fd831d6cea6b38d254",
@@ -126,7 +147,8 @@ Sample HTTP 200 response body for 'pre':
 ```
 
 Sample HTTP 200 response body for 'gal':
-```
+
+```json
 {
     "process": "Galvanized",
     "woID": "gbe732fd831d6cea6b38d256",
@@ -160,11 +182,13 @@ Sample HTTP 200 response body for 'gal':
 ```
 
 Sample HTTP error body:
-```
+
+```json
 {
     "message": "cannot find job in pre-treatment process"
 }
 ```
+
 - "message" - the error message
 
 ### GET /process/:operation
@@ -172,13 +196,16 @@ Sample HTTP error body:
 GetProcMetrics responds with the constant read register containing the values for the metrics of the given operations.
 
 Sample request:
+
+```bash
+curl http://localhost:8080/api/v1/process/1
 ```
-$ curl http://localhost:8080/api/v1/process/1
-```
+
 - "operation" - the operation code of the process, '1' for degreasing #1, '2' for degreasing #2, '3' for pickling #1, '4' for pickling #2, '5' for pickling #3, '6' for galvanization
 
 Sample HTTP 200 response body for process '1' to '5':
-```
+
+```json
 {
     "process": "Pre-treatment",
     "woID": "",
@@ -190,7 +217,8 @@ Sample HTTP 200 response body for process '1' to '5':
 ```
 
 Sample HTTP 200 response body for process '6':
-```
+
+```json
 {
     "process": "Galvanized",
     "woID": "",
@@ -201,11 +229,13 @@ Sample HTTP 200 response body for process '6':
 ```
 
 Sample HTTP error body:
-```
+
+```json
 {
     "message": "unknown operation"
 }
 ```
+
 - "message" - the error message
 
 ### DELETE /job/all
@@ -213,22 +243,27 @@ Sample HTTP error body:
 [DEV ONLY] Remove all jobs from the queues by setting their status to '0' in the PLC.
 
 Sample request:
-```
-$ curl -X DELETE http://localhost:8080/api/v1/job/all
+
+```bash
+curl -X DELETE http://localhost:8080/api/v1/job/all
 ```
 
 Sample HTTP 200 response body:
-```
+
+```json
 {
     "message": "removed all jobs"
 }
 ```
+
 - "message" - the response message
 
 Sample HTTP error body:
-```
+
+```json
 {
     "message": "failed to remove all jobs"
 }
 ```
+
 - "message" - the error message
--- api/updateJob.go
+++ api/updateJob.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -49,37 +50,51 @@ func UpdateJob(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
 			return
 
-		// case 0:
-		// 	// Job finish procedure (r.EventID = 0)
-		// 	val, err := services.GetTagValue("dotzero", models.GetJobMetrics(index))
-		// 	metrics := services.MetricsFields{
-		// 		EmptyBucketWeight: strconv.FormatFloat(val[0], 'f', -1, 64),
-		// 		FullBucketWeight:  strconv.FormatFloat(val[1], 'f', -1, 64),
-		// 		DegreasingTime:    strconv.FormatFloat(val[2], 'f', -1, 64) + "," + strconv.FormatFloat(val[3], 'f', -1, 64) + "," + strconv.FormatFloat(val[4], 'f', -1, 64),
-		// 		PicklingTime:      strconv.FormatFloat(val[5], 'f', -1, 64) + "," + strconv.FormatFloat(val[6], 'f', -1, 64) + "," + strconv.FormatFloat(val[7], 'f', -1, 64),
-		// 		FluxTime:          strconv.FormatFloat(val[8], 'f', -1, 64),
-		// 		Temp:              strconv.FormatFloat(val[9], 'f', -1, 64) + "," + strconv.FormatFloat(val[10], 'f', -1, 64) + "," + strconv.FormatFloat(val[11], 'f', -1, 64) + "," + strconv.FormatFloat(val[12], 'f', -1, 64) + "," + strconv.FormatFloat(val[13], 'f', -1, 64),
-		// 	}
-		// 	// Update job metrics with iShopFloor API
-		// 	if err := services.UpdateMetrics(r.JobID, "1", metrics); err != nil {
-		// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 		return
-		// 	}
-		// 	// Update job status with iShopFloor API
-		// 	err = services.SetJobDone(r.JobID, "1", "", "")
-		// 	if err != nil {
-		// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 		return
-		// 	}
-		// 	// Set job complete code in PLC with WebAccess API
-		// 	err = services.SetTagValue("dotzero", models.W01Status, 0)
-		// 	if err != nil {
-		// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
-		// 		return
-		// 	}
+		case 0:
+			// Get metric value from SCADA system
+			val, err := services.GetTagValue("dotzero", models.GetJobMetrics(index))
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+				return
+			}
+			metrics := services.Route03Metrics{
+				PicklingSmallTime:   strconv.FormatFloat(val[0], 'f', 2, 64) + "," + strconv.FormatFloat(val[1], 'f', 2, 64) + "," + strconv.FormatFloat(val[2], 'f', 2, 64),
+				DegreasingSmallTime: strconv.FormatFloat(val[3], 'f', 2, 64) + "," + strconv.FormatFloat(val[4], 'f', 2, 64) + "," + strconv.FormatFloat(val[5], 'f', 2, 64),
+				HotWaterTime:        strconv.FormatFloat(val[6], 'f', 2, 64) + "," + strconv.FormatFloat(val[7], 'f', 2, 64) + "," + strconv.FormatFloat(val[8], 'f', 2, 64),
+				DegreasingTime:      strconv.FormatFloat(val[9], 'f', 2, 64) + "," + strconv.FormatFloat(val[10], 'f', 2, 64) + "," + strconv.FormatFloat(val[11], 'f', 2, 64),
+				DegreasingWaterTime: strconv.FormatFloat(val[12], 'f', 2, 64) + "," + strconv.FormatFloat(val[13], 'f', 2, 64) + "," + strconv.FormatFloat(val[14], 'f', 2, 64),
+				FluxTime:            strconv.FormatFloat(val[15], 'f', 2, 64) + "," + strconv.FormatFloat(val[16], 'f', 2, 64) + "," + strconv.FormatFloat(val[17], 'f', 2, 64),
+				DryingTime:          strconv.FormatFloat(val[18], 'f', 2, 64) + "," + strconv.FormatFloat(val[19], 'f', 2, 64) + "," + strconv.FormatFloat(val[20], 'f', 2, 64),
+				PicklingTime:        strconv.FormatFloat(val[21], 'f', 2, 64) + "," + strconv.FormatFloat(val[22], 'f', 2, 64) + "," + strconv.FormatFloat(val[23], 'f', 2, 64) + "," + strconv.FormatFloat(val[24], 'f', 2, 64) + "," + strconv.FormatFloat(val[25], 'f', 2, 64) + "," + strconv.FormatFloat(val[26], 'f', 2, 64),
+				PicklingWaterTime:   strconv.FormatFloat(val[27], 'f', 2, 64) + "," + strconv.FormatFloat(val[28], 'f', 2, 64) + "," + strconv.FormatFloat(val[29], 'f', 2, 64),
+				LessFluxTime:        strconv.FormatFloat(val[30], 'f', 2, 64) + "," + strconv.FormatFloat(val[31], 'f', 2, 64) + "," + strconv.FormatFloat(val[31], 'f', 2, 64),
+				TurnOverTime:        strconv.FormatFloat(val[33], 'f', 2, 64) + "," + strconv.FormatFloat(val[34], 'f', 2, 64) + "," + strconv.FormatFloat(val[35], 'f', 2, 64),
+			}
+			jsonstr, err := json.Marshal(metrics)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+				return
+			}
+			// Update job metrics with iShopFloor API
+			if err = services.UpdateRouteMetrics(r.JobID, "3", string(jsonstr)); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+				return
+			}
+			// Update job status with iShopFloor API
+			// err = services.SetJobDone(r.JobID, "1", "", "")
+			// if err != nil {
+			// 	c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+			// 	return
+			// }
+			// Set job complete code in PLC with WebAccess API
+			err = services.SetTagValue("dotzero", models.W01Status, 0)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
+				return
+			}
 
-		// 	c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
-		// 	return
+			c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
+			return
 
 		default:
 			c.JSON(http.StatusBadRequest, gin.H{"code": "u001", "message": "unknown EventID for the pre-treatment process"})
@@ -115,88 +130,101 @@ func UpdateJob(c *gin.Context) {
 			}
 			c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
 			return
-		// case 0:
-		// 	// Job finish procedure (r.EventID = 0)
-		// 	switch index {
-		// 	case 0:
-		// 		// Call iShopFloor API
-		// 		tags := []string{models.Gal01TempAct}
-		// 		wtLilBlu := models.GetTagArray(models.G01WtLilblu, 5)
-		// 		timeLilBlu := models.GetTagArray(models.G01TimeLilblu, 5)
-		// 		tempLilBlu := models.GetTagArray(models.G01TempLilblu, 5)
-		// 		tags = append(tags, wtLilBlu...)
-		// 		tagss := append(timeLilBlu, tempLilBlu...)
-		// 		tags = append(tags, tagss...)
-		// 		val, err := services.GetTagValue("dotzero", tags)
+		case 0:
+			// Job finish procedure (r.EventID = 0)
+			switch index {
+			case 0:
+				// Get metric value from SCADA system
+				tags := []string{}
+				wtLilBlu := models.GetTagArray(models.G01WtLilblu, 5)
+				timeLilBlu := models.GetTagArray(models.G01TimeLilblu, 5)
+				tempLilBlu := models.GetTagArray(models.G01TempLilblu, 5)
+				tags = append(tags, wtLilBlu...)
+				tagss := append(timeLilBlu, tempLilBlu...)
+				tags = append(tags, tagss...)
+				val, err := services.GetTagValue("dotzero", tags)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
+				metrics := services.Route04Metrics{
+					SubBasketWeight: strconv.FormatFloat(val[0], 'f', 2, 64) + "," + strconv.FormatFloat(val[1], 'f', 2, 64) + "," + strconv.FormatFloat(val[2], 'f', 2, 64) + "," + strconv.FormatFloat(val[3], 'f', 2, 64) + "," + strconv.FormatFloat(val[4], 'f', 2, 64),
+					SubBasketTime:   strconv.FormatFloat(val[5], 'f', 2, 64) + "," + strconv.FormatFloat(val[6], 'f', 2, 64) + "," + strconv.FormatFloat(val[7], 'f', 2, 64) + "," + strconv.FormatFloat(val[8], 'f', 2, 64) + "," + strconv.FormatFloat(val[9], 'f', 2, 64),
+					SubBasketTemp:   strconv.FormatFloat(val[10], 'f', 2, 64) + "," + strconv.FormatFloat(val[11], 'f', 2, 64) + "," + strconv.FormatFloat(val[12], 'f', 2, 64) + "," + strconv.FormatFloat(val[13], 'f', 2, 64) + "," + strconv.FormatFloat(val[14], 'f', 2, 64),
+				}
+				jsonstr, err := json.Marshal(metrics)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
+				// Update job metrics with iShopFloor API
+				if err = services.UpdateRouteMetrics(r.JobID, "4", string(jsonstr)); err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
 
-		// 		metrics := services.MetricsFields{
-		// 			FurnaceTtemp:     strconv.FormatFloat(val[0], 'f', -1, 64),
-		// 			LittleBlueWeight: strconv.FormatFloat(val[1], 'f', -1, 64) + "," + strconv.FormatFloat(val[2], 'f', -1, 64) + "," + strconv.FormatFloat(val[3], 'f', -1, 64) + "," + strconv.FormatFloat(val[4], 'f', -1, 64) + "," + strconv.FormatFloat(val[5], 'f', -1, 64),
-		// 			LittleBlueTime:   strconv.FormatFloat(val[6], 'f', -1, 64) + "," + strconv.FormatFloat(val[7], 'f', -1, 64) + "," + strconv.FormatFloat(val[8], 'f', -1, 64) + "," + strconv.FormatFloat(val[9], 'f', -1, 64) + "," + strconv.FormatFloat(val[10], 'f', -1, 64),
-		// 			LittleBlueTemp:   strconv.FormatFloat(val[11], 'f', -1, 64) + "," + strconv.FormatFloat(val[12], 'f', -1, 64) + "," + strconv.FormatFloat(val[13], 'f', -1, 64) + "," + strconv.FormatFloat(val[14], 'f', -1, 64) + "," + strconv.FormatFloat(val[15], 'f', -1, 64),
-		// 		}
+				// defectWt := val[1] - val[0] - val[3]
+				// err = services.SetJobDone(r.JobID, "2", strconv.FormatFloat(val[3], 'f', -1, 64), strconv.FormatFloat(defectWt, 'f', -1, 64))
+				// if err != nil {
+				// 	c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+				// 	return
+				// }
 
-		// 		if err := services.UpdateMetrics(r.JobID, "2", metrics); err != nil {
-		// 			c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 			return
-		// 		}
+				// Mark job complete
+				err = services.SetTagValue("dotzero", models.G01Status, 0)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
+					return
+				}
+			case 1:
+				// Get metric value from SCADA system
+				tags := []string{}
+				wtLilBlu := models.GetTagArray(models.G02WtLilblu, 5)
+				timeLilBlu := models.GetTagArray(models.G02TimeLilblu, 5)
+				tempLilBlu := models.GetTagArray(models.G02TempLilblu, 5)
+				tags = append(tags, wtLilBlu...)
+				tagss := append(timeLilBlu, tempLilBlu...)
+				tags = append(tags, tagss...)
+				val, err := services.GetTagValue("dotzero", tags)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
+				metrics := services.Route04Metrics{
+					SubBasketWeight: strconv.FormatFloat(val[0], 'f', 2, 64) + "," + strconv.FormatFloat(val[1], 'f', 2, 64) + "," + strconv.FormatFloat(val[2], 'f', 2, 64) + "," + strconv.FormatFloat(val[3], 'f', 2, 64) + "," + strconv.FormatFloat(val[4], 'f', 2, 64),
+					SubBasketTime:   strconv.FormatFloat(val[5], 'f', 2, 64) + "," + strconv.FormatFloat(val[6], 'f', 2, 64) + "," + strconv.FormatFloat(val[7], 'f', 2, 64) + "," + strconv.FormatFloat(val[8], 'f', 2, 64) + "," + strconv.FormatFloat(val[9], 'f', 2, 64),
+					SubBasketTemp:   strconv.FormatFloat(val[10], 'f', 2, 64) + "," + strconv.FormatFloat(val[11], 'f', 2, 64) + "," + strconv.FormatFloat(val[12], 'f', 2, 64) + "," + strconv.FormatFloat(val[13], 'f', 2, 64) + "," + strconv.FormatFloat(val[14], 'f', 2, 64),
+				}
+				jsonstr, err := json.Marshal(metrics)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
+				// Update job metrics with iShopFloor API
+				if err = services.UpdateRouteMetrics(r.JobID, "4", string(jsonstr)); err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+					return
+				}
 
-		// 		defectWt := val[1] - val[0] - val[3]
-		// 		err = services.SetJobDone(r.JobID, "2", strconv.FormatFloat(val[3], 'f', -1, 64), strconv.FormatFloat(defectWt, 'f', -1, 64))
-		// 		if err != nil {
-		// 			c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 			return
-		// 		}
+				// defectWt := val[1] - val[0] - val[3]
+				// err = services.SetJobDone(r.JobID, "2", strconv.FormatFloat(val[3], 'f', -1, 64), strconv.FormatFloat(defectWt, 'f', -1, 64))
+				// if err != nil {
+				// 	c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
+				// 	return
+				// }
 
-		// 		// Mark job complete
-		// 		err = services.SetTagValue("dotzero", models.G01Status, 0)
-		// 		if err != nil {
-		// 			c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
-		// 			return
-		// 		}
-		// 	// case 1:
-		// 	// 	// Call iShopFloor API
-		// 	// 	tags := []string{models.Gal02TempSet, models.Gal02TempAct}
-		// 	// 	wtLilBlu := models.GetTagArray(models.G02WtLilblu, 5)
-		// 	// 	timeLilBlu := models.GetTagArray(models.G02TimeLilblu, 5)
-		// 	// 	tempLilBlu := models.GetTagArray(models.G02TempLilblu, 5)
-		// 	// 	tags = append(tags, wtLilBlu...)
-		// 	// 	tagss := append(timeLilBlu, tempLilBlu...)
-		// 	// 	tags = append(tags, tagss...)
-		// 	// 	val, err := services.GetTagValue("dotzero", tags)
-
-		// 	// 	metrics := services.MetricsFields{
-		// 	// 		SetTemperature:    strconv.FormatFloat(val[0], 'f', -1, 64),
-		// 	// 		ActualTemperature: strconv.FormatFloat(val[1], 'f', -1, 64),
-		// 	// 		LittleBlueWeight:  strconv.FormatFloat(val[2], 'f', -1, 64) + "," + strconv.FormatFloat(val[3], 'f', -1, 64) + "," + strconv.FormatFloat(val[4], 'f', -1, 64) + "," + strconv.FormatFloat(val[5], 'f', -1, 64) + "," + strconv.FormatFloat(val[6], 'f', -1, 64),
-		// 	// 		LittleBlueTime:    strconv.FormatFloat(val[7], 'f', -1, 64) + "," + strconv.FormatFloat(val[8], 'f', -1, 64) + "," + strconv.FormatFloat(val[9], 'f', -1, 64) + "," + strconv.FormatFloat(val[10], 'f', -1, 64) + "," + strconv.FormatFloat(val[11], 'f', -1, 64),
-		// 	// 		LittleBlueTemp:    strconv.FormatFloat(val[12], 'f', -1, 64) + "," + strconv.FormatFloat(val[13], 'f', -1, 64) + "," + strconv.FormatFloat(val[14], 'f', -1, 64) + "," + strconv.FormatFloat(val[15], 'f', -1, 64) + "," + strconv.FormatFloat(val[16], 'f', -1, 64),
-		// 	// 	}
-
-		// 	// 	if err := services.UpdateMetrics(r.JobID, "2", metrics); err != nil {
-		// 	// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 	// 		return
-		// 	// 	}
-
-		// 	// 	defectWt := val[1] - val[0] - val[3]
-		// 	// 	err = services.SetJobDone(r.JobID, "2", strconv.FormatFloat(val[3], 'f', -1, 64), strconv.FormatFloat(defectWt, 'f', -1, 64))
-		// 	// 	if err != nil {
-		// 	// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u002", "message": err.Error()})
-		// 	// 		return
-		// 	// 	}
-
-		// 	// 	// Mark job complete
-		// 	// 	err = services.SetTagValue("dotzero", models.G02Status, 0)
-		// 	// 	if err != nil {
-		// 	// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
-		// 	// 		return
-		// 	// 	}
-		// 	default:
-		// 		c.JSON(http.StatusInternalServerError, gin.H{"code": "u001", "message": "update galvanizing job index out of bound"})
-		// 		return
-		// 	}
-		// 	c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
-		// 	return
+				// Mark job complete
+				err = services.SetTagValue("dotzero", models.G02Status, 0)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"code": "u003", "message": err.Error()})
+					return
+				}
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{"code": "u001", "message": "update galvanizing job index out of bound"})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"woID": r.JobID, "queueIndex": index})
+			return
 
 		default:
 			c.JSON(http.StatusBadRequest, gin.H{"code": "u001", "message": "unknown EventID for the galvanizing process"})
